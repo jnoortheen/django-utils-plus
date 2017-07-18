0a3504e0475d010c088d287441b518dbf40801ad
jnoortheen <jnoortheen@gmail.com>
2017-07-18 21:00:20 +0530

    feat: adding AbstractModelIsDeletable

---
+++ utils_plus/abstracts.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+# Copied from this [gist](https://gist.github.com/freewayz/69d1b8bcb3c225bea57bd70ee1e765f8)
+class AbstractModelIsDeletable(models.Model):
+    name = models.CharField(max_length=200, blank=True, null=True, unique=True)
+    description = models.CharField(max_length=200, blank=True, null=True)
+    date_modified = models.DateTimeField(auto_now_add=True)
+
+    def is_deletable(self):
+        # get all the related object
+        for rel in self._meta.get_fields():
+            try:
+                # check if there is a relationship with at least one related object
+                related = rel.related_model.objects.filter(**{rel.field.name: self})
+                if related.exists():
+                    # if there is return a Tuple of flag = False the related_model object
+                    return False, related
+            except AttributeError:  # an attribute error for field occurs when checking for AutoField
+                pass  # just pass as we dont need to check for AutoField
+        return True, None
+
+    class Meta:
+        abstract = True
