237b2d3d135e4703d83eb2485c0033fc30c99aa1
jnoortheen <jnoortheen@gmail.com>
2017-08-23 21:45:41 +0530

    test: update for Url class changes

--- utils_plus/tests/test_router.py
+++ utils_plus/tests/test_router.py
@@ -1,6 +1,6 @@
 import unittest
 
-from utils_plus.url_group import UrlGroup
+from utils_plus.router import Url
 
 
 def view(request):
@@ -9,7 +9,7 @@ def view(request):
 
 class UrlGroupTest(unittest.TestCase):
     def test_nesting_levels(self):
-        with UrlGroup('home', view, 'home') as g:
+        with Url('home', view, 'home') as g:
             g('p1', view, 'report')
             g('p2', view, 'report')
             with g('level1', view, 'sub1'):
@@ -32,7 +32,7 @@ class UrlGroupTest(unittest.TestCase):
         self.assertEqual(next(patterns), '^home/p3/$')
 
     def test_variable_regex(self):
-        with UrlGroup('home', view) as g:
+        with Url('home', view) as g:
             g.pk(view, 'pk')
             g.int('int_var', view, 'int')
             g.str('str_var', view, 'str')
@@ -45,7 +45,7 @@ class UrlGroupTest(unittest.TestCase):
         self.assertEqual(next(patterns), '^home/(?P<var_k>[k]+)/$')
 
     def test_same_level_urls(self):
-        with UrlGroup('home', view) as g:
+        with Url('home', view) as g:
             g.pk(view, 'pk')
         with g('home2', view):
             g.pk(view, 'pk')
@@ -59,7 +59,7 @@ class UrlGroupTest(unittest.TestCase):
         self.assertEqual(next(patterns), '^home3/$')
 
     def test_paths_without_views(self):
-        with UrlGroup('home') as g:
+        with Url('home') as g:
             with g.pk():
                 g('edit', view)
             with g.int('integer'):
@@ -84,7 +84,7 @@ class UrlGroupTest(unittest.TestCase):
 
     def test_include_patterns_function(self):
         raise NotImplementedError
-        # with UrlGroup('home') as g:
+        # with Url('home') as g:
         #     with g.pk():
         #         g.incl('edit', 'url_conf_module')
         # with g('home2'):
@@ -101,7 +101,7 @@ class UrlGroupTest(unittest.TestCase):
 
     def test_paths_that_starts_with_a_blank_root(self):
         # raise NotImplementedError
-        with UrlGroup('', view) as g:
+        with Url('', view) as g:
             with g('home'):
                 with g.pk():
                     g('edit', view)
