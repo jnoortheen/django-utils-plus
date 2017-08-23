0e39c9b2a5d8162f386e460cf1d6c78a22f0b528
jnoortheen <jnoortheen@gmail.com>
2017-08-23 21:45:52 +0530

    doc: add doc for ChoicesEnum

--- utils_plus/choices.py
+++ utils_plus/choices.py
@@ -2,6 +2,23 @@ import enum
 
 
 class ChoicesEnum(enum.Enum):
+    """
+        Enumerator class for use with the django ORM choices field
+
+    Usage:
+        create a class with choices as fields like this
+
+        # choices.py
+        class COLOR_TYPE(ChoicesEnum):
+            red = 'Green'
+            green = 'Red'
+
+        # models.py
+        ...
+        models.CharField('Color', choices=COLOR_TYPE.choices(), default=COLOR_TYPE.red.name, max_length=10)
+        ...
+    """
+
     @classmethod
     def choices_gen(cls):
         for attr in cls:
