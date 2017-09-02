dc06aa5b7baa6a180aee76f331fed77003a0a109
jnoortheen <jnoortheen@gmail.com>
2017-09-02 14:12:00 +0530

    chore: exclude test project from being included

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 import os
 from setuptools import find_packages, setup
 
+import utils_plus
+
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
+print(find_packages())
 setup(
     name='django-utils-plus',
-    version='0.2',
-    packages=find_packages(),
+    version=utils_plus.__version__,
+    packages=find_packages(exclude=['test_proj*', 'test_app*']),
     include_package_data=True,
     license='LGPL License',
     description='A Django with small set of utilities for urls, viewsets, commands and more',
