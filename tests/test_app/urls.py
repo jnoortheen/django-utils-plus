8cba8d16b2ff26ef10ac5a6d66f89a6f93474fd4
jnoortheen <jnoortheen@gmail.com>
2017-10-02 00:24:44 +0530

    test: add more tests for mixins

--- tests/test_app/urls.py
+++ tests/test_app/urls.py
@@ -1,7 +1,9 @@
 from utils_plus.router import Url
 from utils_plus.views import return_path_view
+from .views import CreateUpdateAuthorView
 
 with Url('blog') as u:
     u.slug(return_path_view, 'blog-slug')
+    u.incl(CreateUpdateAuthorView.urls(), namespace='author', prefix='author_profile')
 
 urlpatterns = u.urlpatterns
--- tests/test_app/views.py
+++ tests/test_app/views.py
@@ -1,1 +1,6 @@
-from django.shortcuts import render
+from tests.test_app.models import Author
+from utils_plus.views import CreateUpdateView
+
+
+class CreateUpdateAuthorView(CreateUpdateView):
+    model = Author
