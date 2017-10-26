c97501249743b62f5629c213fa944cb8dcf35161
jnoortheen <jnoortheen@gmail.com>
2017-10-26 11:57:27 +0530

    style:

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-print(find_packages())
+
 setup(
     name='django-utils-plus',
     version=utils_plus.__version__,
