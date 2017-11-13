1f85d1dbda3354b93441f3755e450cd812d72fac
jnoortheen <jnoortheen@gmail.com>
2017-11-13 15:02:42 +0530

    feat: add a debug  view

--- utils_plus/views.py
+++ utils_plus/views.py
@@ -1,5 +1,10 @@
 from __future__ import absolute_import
-from django.http import HttpResponse
+
+import mimetypes
+
+from django.http import HttpResponse, FileResponse
+from django.views import View
+
 from .mixins import CreateUpdateMixin
 
 
@@ -20,6 +25,19 @@ def return_path_view(request, *args, **kwargs):
     b"Requested / with Args: ('arg1', 'arg2') {'kwarg': 1}"
     """
     return HttpResponse("Requested " + request.path + " with Args: {} {}".format(args, kwargs))
+
+
+class FileResponseView(View):
+    def __index__(self, file_path):
+        self.file_path = file_path
+
+    def get(self, request):
+        content_type, encoding = mimetypes.guess_type(self.file_path)
+        f = open(self.file_path, "rb")
+        response = FileResponse(f, content_type=content_type)
+        if encoding:
+            response["Content-Encoding"] = encoding
+        return response
 
 
 class CreateUpdateView(CreateUpdateMixin):
